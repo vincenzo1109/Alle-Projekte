cafa7bb3d3f5aad9e404c8a461441c93230fda73
vincenzo1109 <github.bienert@dfgh.net>
2025-11-18 21:14:08 +0100

    right loading of the saved objects

--- planzen_app/lib/main.dart
+++ planzen_app/lib/main.dart
@@ -6,8 +6,18 @@ import 'achievements.dart';
 import 'impressum.dart';
 import 'plants_overview.dart';
 import 'package:intl/intl.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+part 'main.g.dart';
+
+String objectPlantKey = 'plantObjectSaves';
+String dataKey = 'dataSaves';
+
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(AllPlantsAdapter());
+  await Hive.openBox(dataKey);
+  await Hive.openBox(objectPlantKey);
   runApp(const MyApp());
 }
 
@@ -46,7 +56,7 @@ class MyAppState extends State<MyApp> {
             fontSize: 24,
             fontWeight: FontWeight.bold,
           ),
-          iconTheme: IconThemeData(size: 30), // Drawer + Actions Icons
+          iconTheme: IconThemeData(size: 30),
         ),
       ),
 
@@ -79,12 +89,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<AllPlants> plantsListHomeScreen = [];
+  List<AllPlants> plantsListHomeScreenFirstStart = [];
+  List<AllPlants> plantsListHomeScreenNotFirstStart = [];
+
+  var plantDataBox = Hive.box(dataKey);
+  var plantObjectBox = Hive.box(objectPlantKey);
+
+  void getObjectList() {
+    var objectList = (plantObjectBox.get('plantObjectList'));
+    if (objectList != null) {
+      plantsListHomeScreenNotFirstStart = (objectList as List)
+          .cast<AllPlants>();
+    } else {
+        print('Fehler beim getObjectList()');
+    }
+  }
 
   @override
   void initState() {
     super.initState();
-    plantsListHomeScreen = List.from(myPlants);
+    plantsListHomeScreenFirstStart = myPlants;
+    var openend = plantDataBox.get('alreadyOpened', defaultValue: false);
+    openend? getObjectList() : {};
   }
 
   @override
@@ -140,6 +166,13 @@ class _MyHomePageState extends State<MyHomePage> {
               },
             ),
             ListTile(
+              leading: Icon(Symbols.add_2_rounded),
+              title: Text('Eine weitere Pflanze hinzufügen'),
+              onTap: () {
+                Navigator.pushNamed(context, '/impressum');
+              },
+            ),
+            ListTile(
               leading: Icon(Symbols.settings),
               title: Text('Einstellungen'),
               onTap: () {
@@ -156,16 +189,23 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      body: ListView(children: plantsHomescreen()),
+      body: plantDataBox.get('alreadyOpened', defaultValue: false)
+          ? ListView(
+              children: plantsHomescreen(plantsListHomeScreenNotFirstStart),
+            )
+          : ListView(
+              children: plantsHomescreen(plantsListHomeScreenFirstStart),
+            ),
     );
   }
 
-  List<Widget> plantsHomescreen() {
+  List<Widget> plantsHomescreen(whichList) {
     List<Widget> plantsNext = [];
-
+    var boxHiveOpen = Hive.box(dataKey);
+    boxHiveOpen.put('alreadyOpened', true);
 
     void sortList() {
-      plantsListHomeScreen.sort((a, b) {
+      whichList.sort((a, b) {
         var dueA = DateFormat(
           'dd.MM.yyyy',
         ).parse(a.lasttime).add(Duration(days: a.interval));
@@ -178,7 +218,7 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 
     sortList();
-    for (var plantvar in plantsListHomeScreen) {
+    for (var plantvar in whichList) {
       DateTime savedDate = DateFormat('dd.MM.yyyy').parse(plantvar.lasttime);
       DateTime dueDate = savedDate.add(Duration(days: plantvar.interval));
       int doItInDays = dueDate.difference(now).inDays;
@@ -198,33 +238,67 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           subtitle: Center(
             child: Text(
-              dueDate.isBefore(now)
-                  ? (expiredSince == 1
-                        ? 'Seit $expiredSince Tag überfällig'
-                        : 'Seit $expiredSince Tagen überfällig')
-                  : (doItInDays == 1
-                        ? 'In $doItInDays Tagw anfällig'
-                        : 'In $doItInDays Tagen anfällig'),
+              dueDate.isAtSameMomentAs(now)
+                  ? 'Muss heute gemacht werden'
+                  : (dueDate.isBefore(now)
+                        ? (expiredSince == 1
+                              ? 'Seit $expiredSince Tag überfällig'
+                              : 'Seit $expiredSince Tagen überfällig')
+                        : (doItInDays == 1
+                              ? 'In $doItInDays Tag anfällig'
+                              : 'In $doItInDays Tagen anfällig')),
               textAlign: TextAlign.center,
             ),
           ),
           trailing: IconButton(
             onPressed: () {
               setState(() {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(
-                      'Wieder etwas erledigt :)',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold,
+                if (dueDate.difference(now).inDays <= 4) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      showCloseIcon: true,
+                      action: SnackBarAction(
+                        label: 'Rückgängig',
+                        onPressed: () {
+                          plantvar.lasttime = lastLastTimeFunction(
+                            plantvar.lasttime,
+                            plantvar.interval,
+                          );
+                          plantvar.lastLastTime = lastLastTimeFunction(
+                            plantvar.lastLastTime,
+                            plantvar.interval,
+                          );
+                        },
+                      ),
+                      content: Text(
+                        'Wieder etwas erledigt :)',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ),
-                );
-                plantvar.lasttime = DateFormat('dd.MM.yyyy').format(now);
-                sortList();
+                  );
+                  plantvar.lastLastTime = plantvar.lasttime;
+                  plantvar.lasttime = DateFormat('dd.MM.yyyy').format(now);
+                  hivePutMyPlantsList(whichList);
+                  sortList();
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      showCloseIcon: true,
+                      content: Text(
+                        '${plantvar.name} ${plantvar.whatToDo} hat noch Zeit :-)',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  );
+                }
               });
             },
             icon: Icon(Icons.check),
@@ -232,6 +306,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       );
     }
+    getObjectList();
     return plantsNext;
   }
 }
@@ -244,13 +319,12 @@ String formattedDate = '07.11.2025'; // Vorrübergehende Lösung
 
 List<AllPlants> myPlants = [];
 
-void existingPlants() {
-  addPlantMyPlants('Rose', 2, '08.11.2025', 'düngen', 14);
-  addPlantMyPlants('Himbeere', 4, formattedDate, 'verschneiden', 21);
-  addPlantMyPlants('Erdbeerem', 3, '31.08.2025', 'ernten', 365);
-  addPlantMyPlants('Alle Blumen', 4, formattedDate, 'gießen', 7);
-  addPlantMyPlants('Löwenzahn', 1, '01.11.2025', 'ausrotten', 7);
-  addPlantMyPlants('Hanf-Pflanze', 1, '02.11.2025', 'verarbeiten', 90);
+lastLastTimeFunction(lasttime, interval) {
+  DateTime lastLastTime = DateFormat(
+    'dd.MM.yyyy',
+  ).parse(lasttime).subtract(Duration(days: interval));
+  String lastLastTimeString = lastLastTime.toString();
+  return lastLastTimeString;
 }
 
 void addPlantMyPlants(
@@ -263,17 +337,64 @@ void addPlantMyPlants(
   int alter = alterinc;
   String name = nameinc;
 
-  AllPlants plant = AllPlants(name, alter, lasttime, whatToDo, interval);
+  String lastLastTimeString = lastLastTimeFunction(lasttime, interval);
+
+  AllPlants plant = AllPlants(
+    name,
+    alter,
+    lastLastTimeString,
+    lasttime,
+    whatToDo,
+    interval,
+  );
   myPlants.add(plant);
 }
 
+@HiveType(typeId: 0)
 class AllPlants {
+  @HiveField(0)
   String name;
+
+  @HiveField(1)
   int alter;
+
+  @HiveField(2)
+  String lastLastTime;
+
+  @HiveField(3)
   String lasttime;
-  String whatToDo;
+
+  @HiveField(4)
   int interval;
 
-  //constructor
-  AllPlants(this.name, this.alter, this.lasttime, this.whatToDo, this.interval);
+  @HiveField(5)
+  String whatToDo;
+
+  AllPlants(
+    this.name,
+    this.alter,
+    this.lastLastTime,
+    this.lasttime,
+    this.whatToDo,
+    this.interval,
+  );
+}
+
+void hivePutMyPlantsList(List<AllPlants> saveList) {
+  var boxHive = Hive.box(objectPlantKey);
+  boxHive.put('plantObjectList', saveList);
+}
+
+void existingPlants() {
+  var boxHiveOpen = Hive.box(dataKey);
+  bool alreadyOpened = boxHiveOpen.get('alreadyOpened', defaultValue: false);
+  if (!alreadyOpened) {
+    addPlantMyPlants('Rose', 2, '08.11.2025', 'düngen', 14);
+    addPlantMyPlants('Himbeere', 4, formattedDate, 'verschneiden', 21);
+    addPlantMyPlants('Erdbeeren', 3, '31.08.2025', 'ernten', 3);
+    addPlantMyPlants('Alle Blumen', 4, formattedDate, 'gießen', 7);
+    addPlantMyPlants('Löwenzahn', 1, '01.11.2025', 'ausrotten', 7);
+    addPlantMyPlants('Hanf-Pflanze', 1, '02.11.2025', 'verarbeiten', 90);
+    hivePutMyPlantsList(myPlants);
+  }
 }
